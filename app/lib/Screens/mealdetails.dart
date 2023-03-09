3530636da9b2717ac15723cad45e1c77cbda9a85
BildadOwuor <bildadowuor@gmail.com>
2023-03-09 19:11:00 +0300

    updates and fixes

--- app/lib/Screens/mealdetails.dart
+++ app/lib/Screens/mealdetails.dart
@@ -34,6 +34,120 @@ class _MealDetailsScreenState extends State<MealDetailsScreen> {
     );
   }
 
+  Widget buildmealDetailContainer(
+    context,
+    String mealTitle,
+    String mealCategory,
+    String hotelSelling,
+    String rating,
+    String deliveryTime,
+    String mealAmountForTwo,
+  ) {
+    return Container(
+      transform: Matrix4.translationValues(0.0, -50.0, 0.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            mealTitle,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            mealCategory,
+            style: const TextStyle(
+              color: Colors.grey,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [const Icon(Icons.location_on), Text(hotelSelling)],
+          ),
+          TextButton(
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: const BorderSide(color: Colors.red),
+                ),
+              ),
+              backgroundColor: const MaterialStatePropertyAll(
+                Colors.red,
+              ),
+            ),
+            onPressed: () {},
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: const Text(
+                "Book Table",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    const Icon(Icons.star_outline),
+                    Text(
+                      rating,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+                const Divider(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "$deliveryTime min",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      "Delivery Time",
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                ),
+                const Divider(),
+                Column(
+                  children: [
+                    Text(
+                      "Ksh $mealAmountForTwo",
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const Text(
+                      "Order For Two",
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget buildLeading(context, IconData icon, String route) {
     return IconButton(
       onPressed: () {
@@ -66,16 +180,23 @@ class _MealDetailsScreenState extends State<MealDetailsScreen> {
               const AssetImage("assets/Images/pizza.jpg"),
             ),
             Container(
+              height: 90,
               transform: Matrix4.translationValues(0.0, -60.0, 0.0),
               child: const CircleAvatar(
                 radius: 50,
                 backgroundColor: Colors.red,
                 child: CircleAvatar(
                   foregroundImage: AssetImage("assets/Images/pizza.jpg"),
-                  radius: 48,
+                  radius: 40,
                 ),
               ),
             ),
+            buildmealDetailContainer(
+                "", "", "", "", "", "", ""), //!TODO:get meal details
+
+            const RecommendedWidget(
+                mealCategoryId:
+                    ''), //!TODO : get current meal category Id to populated other recomended meals
           ],
         ),
       ),
@@ -83,16 +204,24 @@ class _MealDetailsScreenState extends State<MealDetailsScreen> {
       floatingActionButton: SizedBox(
         width: 150,
         child: ElevatedButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: const BorderSide(color: Colors.red),
+              ),
+            ),
+          ),
           onPressed: () {
             Navigator.of(context).pushNamed(MealScreen.routeName);
           },
           child: Row(children: const [
-            Icon(Icons.category, color: Colors.white),
+            Icon(Icons.fastfood, color: Colors.white),
             SizedBox(
               width: 10,
             ),
             Text(
-              "Browse all Menu",
+              "Browse Menu",
               style: TextStyle(
                 color: Colors.white,
               ),
@@ -131,4 +260,22 @@ class CurveImage extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class RecommendedWidget extends StatefulWidget {
+  final String mealCategoryId;
+  const RecommendedWidget({
+    Key? key,
+    required this.mealCategoryId,
+  }) : super(key: key);
+
+  @override
+  State<RecommendedWidget> createState() => _RecommendedWidgetState();
+}
+
+class _RecommendedWidgetState extends State<RecommendedWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
