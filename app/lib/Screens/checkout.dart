2dc99d2a9c607552ac7b2915f7b5c9bd47bf6253
BildadOwuor <bildadowuor@gmail.com>
2023-03-15 21:58:36 +0300

    updates and fixes

--- app/lib/Screens/checkout.dart
+++ app/lib/Screens/checkout.dart
@@ -33,7 +33,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
         title: const Text('Checkout'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(9),
+        padding: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -44,27 +44,74 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
               child: Card(
                 shape: const StadiumBorder(
                   side: BorderSide(
-                    color: Colors.black,
-                    width: 2.0,
+                    color: Colors.white,
+                    width: 1.0,
                   ),
                 ),
                 color: Colors.grey[200],
                 elevation: 0,
-                child: SizedBox(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
                   height: 40,
-                  child: Column(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(address != null
-                          ? 'Addres: $address'
-                          : 'Retrieving location...')
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Text("Delivery:"),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            address ?? 'Retrieving location...',
+                            style: const TextStyle(color: Colors.red),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          Text(
+                            "Change Location",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_outlined,
+                            size: 14,
+                            color: Colors.red,
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
               ),
             ),
-            const Text(
-              'Order Summary',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            Container(
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Order Summary',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text("Items:"),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "${cartItems.length}",
+                        style: const TextStyle(fontSize: 14, color: Colors.red),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 16),
             Expanded(
@@ -82,15 +129,48 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
               ),
             ),
             const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Total:',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            Container(
+              color: Colors.grey[100],
+              height: 150,
+              padding: const EdgeInsets.all(10),
+              child: Card(
+                elevation: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Total:',
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Ksh $totalAmount",
+                            style: const TextStyle(
+                              color: Colors.orangeAccent,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Delivery date"),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text("Select date"),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-                Text("Ksh $totalAmount")
-              ],
+              ),
             ),
             const SizedBox(height: 16),
             ElevatedButton(
--- app/lib/Screens/user_location.dart
+++ app/lib/Screens/user_location.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
+class UserLocationMap extends StatefulWidget {
+  const UserLocationMap({super.key});
+
+  @override
+  State<UserLocationMap> createState() => _UserLocationMapState();
+}
+
+class _UserLocationMapState extends State<UserLocationMap> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
